ff42aa42b3d11f43d54520cc1283f47cf002bef5
ShadowInSpace <ivan.yackovec@gmail.com>
2023-04-13 00:54:57 +0300

    Update SecurityConfig.java

--- src/main/java/go/ti/goItHW12/security/SecurityConfig.java
+++ src/main/java/go/ti/goItHW12/security/SecurityConfig.java
@@ -1,12 +1,9 @@
 package go.ti.goItHW12.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
